4d6814f43071fa94f2b32eab650102cbc40085f2
gabrielafabricioads <gabrielafabricio59@gmail.com>
2025-02-27 09:51:05 -0300

    exs

---
+++ ex1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+<form action="index.php" method="get">
+        <fieldset>
+            <legend>Digite a sua nota:</legend>
+        <label for="nota">Nota</label>
+        <input type="text" name="nota" id="nota" placeholder="Nota do aluno" required>
+        <br>
+
+          <br>
+        <input type="submit" value="Enviar">
+        </fieldset>
+    </form>
+
+    <?php
+    //Elabore um programa que irá receber a nota do aluno de 0 a 10, e exiba se o aluno está aprovado, reprovado ou de recuperação.
+
+//- Nota < 6 reprovado
+//- Nota entre 6 e 7 recuperação
+//- Nota > 7 aprovado
+     if (($_GET['nota']<6)){
+      echo "Reprovado!";
+        
+    } 
+    else if (($_GET['nota']==6)){
+        echo "De recuperação!";
+    }
+    else if (($_GET['nota']>7 && ($_GET['nota']<=10))){
+        echo "Aprovado!";
+    }
+    else{
+        echo "Digite novamente!";
+    }
+    ?>
+</body>
+</html>
---
+++ ex7.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7</title>
+</head>
+<body>
+<form action="index.php" method="get">
+        <fieldset>
+            <legend>Digite a sua idade para saber se você poderá votar:</legend>
+        <label for="idade">idade</label>
+        <input type="text" name="idade" id="idade" placeholder="idade" required>
+        <br>
+
+          <br>
+        <input type="submit" value="Enviar">
+        </fieldset>
+    </form>
+
+    <?php
+    //Faça um programa que irá receber o ano de nascimento de um usuário e exiba se ele é maior ou menor de idade.
+     if (($_GET['idade']<16)){
+      echo "Não tem idade suficiente para votar!";
+        
+    } 
+    else if (($_GET['idade']>=16 && ($_GET['idade']<18))){
+        echo "Você pode votar, mas não é obrigatório!";
+    }
+    else if (($_GET['idade']>=18 && ($_GET['idade']<=69))){
+        echo "Seu voto é obrigatório!";
+    }
+    else if (($_GET['idade']>=70)){
+        echo "Você pode votar, mas o seu voto não é obrigatório! ";
+    }
+    else{
+        echo "Digite novamente!";
+    }
+    ?>
+    
+
+
+</body>
+</html>
